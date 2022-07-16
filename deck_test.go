579cd30761d41569a49b7e85b2d327b130fde9b3
Madhav Sharma <madhav9620@gmail.com>
2022-07-16 19:22:05 +0530

    add test for io files

--- deck_test.go
+++ deck_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestNewDeck(t *testing.T) {
 	d := newDeck()
@@ -15,4 +18,17 @@ func TestNewDeck(t *testing.T) {
 	if d[len(d)-1] != "Four of Clubs" {
 		t.Errorf("Expected last card of Four of Clubs, but got %v", d[0])
 	}
+}
+
+func TestSaveToDeckAndNewDeckFromFile(t *testing.T) {
+	os.Remove(("_decktesting"))
+
+	deck := newDeck()
+	deck.saveToFile("_decktesting")
+
+	loadedDeck := newDeckFromFile("_decktesting")
+	if len(loadedDeck) != 16 {
+		t.Errorf("Expected Deck length of 16, but got %v", len(loadedDeck))
+	}
+	os.Remove("_decktesting")
 }
