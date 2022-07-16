f16236702e70921c3eded90cc51792dff6a6697a
Madhav Sharma <madhav9620@gmail.com>
2022-07-16 19:13:58 +0530

    add last card test for newDeck

--- deck_test.go
+++ deck_test.go
@@ -7,4 +7,12 @@ func TestNewDeck(t *testing.T) {
 	if len(d) != 16 {
 		t.Errorf("Expected Deck length of 16, but got %v", len(d))
 	}
+
+	if d[0] != "One of Diamond" {
+		t.Errorf("Expected first card of One of Daimond, but got %v", d[0])
+	}
+
+	if d[len(d)-1] != "Four of Clubs" {
+		t.Errorf("Expected last card of Four of Clubs, but got %v", d[0])
+	}
 }
