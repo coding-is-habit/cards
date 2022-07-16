31d7bee75116f39fbfd99e62ff7e7de070025761
Madhav Sharma <madhav9620@gmail.com>
2022-07-16 15:48:38 +0530

    save deck to file

--- deck.go
+++ deck.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"strings"
 )
 
@@ -36,4 +37,8 @@ func deal(d deck, handSize int) (deck, deck) {
 
 func (d deck) toString() string {
 	return strings.Join([]string(d), ",")
+}
+
+func (d deck) saveToFile(fileName string) error {
+	return ioutil.WriteFile(fileName, []byte(d.toString()), 06666)
 }
--- main.go
+++ main.go
@@ -1,7 +1,5 @@
 package main
 
-import "fmt"
-
 func main() {
 	cards := newDeck()
 
@@ -9,5 +7,6 @@ func main() {
 
 	// hand.print()
 	// remainingCards.print()
-	fmt.Println(cards.toString())
+	// fmt.Println(cards.toString())
+	cards.saveToFile("my_cards")
 }
