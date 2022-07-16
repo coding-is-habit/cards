00a80b3e198fbd5d1ac3b41b47a29ae0f08fda7a
Madhav Sharma <madhav9620@gmail.com>
2022-07-16 15:45:01 +0530

    convert deck to string

--- deck.go
+++ deck.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 // create new type of 'deck'
 // which is slice of strings
@@ -29,4 +32,8 @@ func newDeck() deck {
 
 func deal(d deck, handSize int) (deck, deck) {
 	return d[:handSize], d[handSize:]
+}
+
+func (d deck) toString() string {
+	return strings.Join([]string(d), ",")
 }
--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
+import "fmt"
+
 func main() {
 	cards := newDeck()
 
-	hand, remainingCards := deal(cards, 5)
+	// hand, remainingCards := deal(cards, 5)
 
-	hand.print()
-	remainingCards.print()
+	// hand.print()
+	// remainingCards.print()
+	fmt.Println(cards.toString())
 }
